d109a8544b364d7135ea935273cd1a12bb3e8c9c
tommy <22.hwang.jiwon@gmail.com>
2022-04-16 15:59:54 +0900

    zadadadasasas

--- IM_3_예외처리와 파일 입출력.py
+++ IM_3_예외처리와 파일 입출력.py
@@ -2,12 +2,20 @@
 
 # [예외처리]
 # : 프로그램 실행 중에 발생하는 예외를 미연에 방지하는 것.
-#
-# ## 예외처리가 필요한 이유 예시:  계산기 프로그램
+
+## 예외처리가 필요한 이유 예시:  계산기 프로그램
 # num1 = float(input("소수점 넣어"))
 # num2 = float(input("소수점 넣어"))
 # andd = num1 + num2
 # print(andd)
+# try:
+#     num1 = float(input("소수점 넣어"))
+#     num2 = float(input("소수점 넣어"))
+# except ValueError as e:
+#     print("숫자만 넣으세요.")
+# else:
+#     andd = num1 + num2
+#     print(andd)
 # ## 예외처리 연습문제: 환율 계산 문제
 # won = input("원화 금액 입력")
 # dallor = input("달러 금액 입력")
@@ -22,32 +30,61 @@
 #     print("dpfj qkftod dksgka")
 # finally:
 #     print("예외가 발생하건 안하건 한다")
-# ## 에러 발생시키기 연습문제
+## 에러 발생시키기 연습문제
+
+
+#
+# st =input("입력하세요")
+# if st == "fuck":
+#     raise Exception("skQmsakf")
+    # except skQmsakf
 #
 #
-
-st =input("입력하세요")
-if st == "fuck":
-    raise Exception("skQmsakf skQk")
-
-
-## 예외처리 Mission: 영어단어 공부하기 문제를 'while문 + 예외 처리'로 풀어보자
-wordlist = ["apple","banna","bababababababa"]
-print(wordlist[0])
-a = input()
-while True:
+# # 예외처리 Mission: 영어단어 공부하기 문제를 'while문 + 예외 처리'로 풀어보자
+#  wordlist = ["apple","banna","bababababababa"]
+# # # print(wordlist[0])
+# i=0
+# # a = input()
+# score = 0
+# while True:
+#     try:
+#         user = input(wordlist[i]+"\n")
+#     except:
+#         break
+#     if user == wordlist[1]:
+#         score += 1
+#     i += 1
+# print(score)
 # [파일 입출력]
-## 파일 입출력 연습문제1: 파일 쓰기(w: 새로쓰기 혹은 덮어쓰기)
+# 파일 입출력 연습문제1: 파일 쓰기(w: 새로쓰기 혹은 덮어쓰기)
+file = open('./fuck.txt','w',encoding="utf-8")
+for i in range(1,11):
+    file.write(f"{i}번째 줄입니다.\n")
+file.close()
+# 파일 입출력 연습문제2: 파일 추가하기(a: 추가하기)
+# file = open('./fuck.txt','a',encoding="utf-8")
+# for i in range(10,21):
+#     file.write(f"{i}번째 줄입니다.\n")
+# file.close()
 
+# 파일 입출력 연습문제3: 파일 읽기(r: 읽기)
+# way1) readline 함수 사용(한 줄 씩 읽어오기)
+# file = open('./fuck.txt','r',encoding="utf-8")
+# while True:
+#     data = file.readline()
+#     print(data)
+#     if data == "":
+#         break
+# file.close()
 
-## 파일 입출력 연습문제2: 파일 추가하기(a: 추가하기)
-
-
-## 파일 입출력 연습문제3: 파일 읽기(r: 읽기)
-## way1) readline 함수 사용(한 줄 씩 읽어오기)
-
-
-## way2) readlines 함수 사용(모든 줄을 읽어와, 각 줄을 리스트의 요소로 반환)
-
-
-## way3) read 함수 사용(문서 전체를 하나의 문자열로 가져오기)
+# way2) readlines 함수 사용(모든 줄을 읽어와, 각 줄을 리스트의 요소로 반환)
+# file = open('./fuck.txt','r',encoding="utf-8")
+# data = file.readlines()
+# print(data)
+# for one in data:
+#     print(one)
+# file.close()
+# way3) read 함수 사용(문서 전체를 하나의 문자열로 가져오기)
+file = open('./fuck.txt','r',encoding="utf-8")
+data = file.read()
+print(data)
